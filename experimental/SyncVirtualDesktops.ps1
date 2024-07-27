89e79ded6f0e674593575f54ff78e34f336a02fd
Timothy Johnson <tandrewjohnson@outlook.com>
2024-07-27 13:09:02 -0400

    Fix error installing nuget provider in SyncVirtualDesktops script

--- experimental/SyncVirtualDesktops.ps1
+++ experimental/SyncVirtualDesktops.ps1
@@ -8,8 +8,10 @@ $currentWinVer = (Get-CimInstance Win32_OperatingSystem).version
 $settingsFile = "$((Get-Item $PSCommandPath).BaseName).dat"
 $settingsUpdated = $false
 if (-Not (Get-Module -ListAvailable -Name VirtualDesktop)) {
-    Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
-    Install-Module -Name VirtualDesktop -Force -Scope CurrentUser
+    if (-Not (Get-PackageProvider -ListAvailable | Where-Object { $_.Name -eq "NuGet" })) {
+        Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
+    }
+    Install-Module -Name VirtualDesktop -AllowClobber -Force -Scope CurrentUser
     $settingsUpdated = $true
 } elseif (-Not (Test-Path -Path $settingsFile -PathType Leaf) -Or ($currentWinVer -Ne (Import-Clixml -Path $settingsFile))) {
     Update-Module -Name VirtualDesktop -Force
