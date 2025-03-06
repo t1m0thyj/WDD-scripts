6882ed7466085e0ec7dabf626283611df3e42921
Francesco Santone <79326795+KrakenByte27@users.noreply.github.com>
2025-03-06 14:30:18 +0100

    Update ChangeFullWindowsTheme.ps1
    I integrated the code "fix" made by @t1m0thyj for the other two system and app scripts. This way the script should be usable on both W10 and W11. I cannot test it on W10 so I cannot be sure.

--- stable/ChangeFullWindowsTheme.ps1
+++ stable/ChangeFullWindowsTheme.ps1
@@ -3,28 +3,26 @@ $params = $Input | ConvertFrom-Json
 $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
 $dwordValue = 1 - $(If ($params.themeMode -Eq 0) {$params.daySegment2} Else {$params.themeMode - 1})
 
-if (Test-Path $registryPath) {
-    Set-ItemProperty -Path $registryPath -Name "AppsUseLightTheme" -Value $dwordValue -Type DWORD -Force
-    Set-ItemProperty -Path $registryPath -Name "SystemUsesLightTheme" -Value $dwordValue -Type DWORD -Force
+if (Test-Path $registryPath)
+{
+    New-ItemProperty -Path $registryPath -Name "AppsUseLightTheme" -Value $dwordValue -PropertyType DWORD -Force | Out-Null
 }
 
-# Call Windows theme refresh
-Add-Type -TypeDefinition @"
+if ([System.Environment]::OSVersion.Version.Build -ge 22000)
+{
+    # Call Windows theme refresh
+    Add-Type -TypeDefinition @"
 using System;
 using System.Runtime.InteropServices;
 
 public class Win32Utils {
     [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-    public static extern int SendMessageTimeout(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam, int fuFlags, int uTimeout, out IntPtr lpdwResult);
+    public static extern int SendMessageTimeout(IntPtr hWnd, int Msg, IntPtr wParam, string lParam, int fuFlags, int uTimeout, out IntPtr lpdwResult);
 }
 "@ -PassThru
 
-$HWND_BROADCAST = [IntPtr]::Zero -bor 0xFFFF
-$WM_SETTINGCHANGE = 0x1A
-$SPI_SETCLIENTAREAANIMATION = 0x1043
-
-$param = [System.Runtime.InteropServices.Marshal]::StringToHGlobalUni("ImmersiveColorSet")
-
-$null = [Win32Utils]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [IntPtr]::Zero, $param, 2, 5000, [ref]([IntPtr]::Zero))
-
-[System.Runtime.InteropServices.Marshal]::FreeHGlobal($param)
+    $HWND_BROADCAST = [IntPtr]0xFFFF
+    $WM_SETTINGCHANGE = 0x1A
+    $SMTO_ABORTIFHUNG = 0x2
+    [Win32Utils]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [IntPtr]::Zero, "ImmersiveColorSet", $SMTO_ABORTIFHUNG, 5000, [ref]([IntPtr]::Zero))
+}
